75791d608dd389130d5583a26b10d9e2b64fc164
Asadbek Rashidov <rashidovasadbek264@gmail.com>
2023-07-15 15:22:18 +0500

    Add project files.

---
+++ Email_checker/Program.cs
@@ -0,0 +1,48 @@
+﻿using System.Security.Cryptography;
+
+Console.WriteLine("Emailingizni kiriting:");
+string email = Console.ReadLine();
+bool  check = false;
+string temp = "";
+string lowerlater = "";
+for (int j = 0; j < email.Length; j++)
+{
+    if ((email[j] >= 'A' && email[j] <= 'Z') || (email[j] >= 'a' && email[j] <= 'z') || email[j] == '.' || email[j] == '@')
+    {
+        lowerlater += email[j].ToString().Replace(email[j], char.ToLower(email[j]));
+    }
+}
+
+
+for  (int i = 0;i < lowerlater.Length; i++)
+{
+    
+    if ((char)(lowerlater[i]) > 96 && (char)(lowerlater[i]) < 123) 
+    {
+            check = true;
+    }
+    else if ((char)(lowerlater[i]) > 47 && (char)(lowerlater[i]) < 59)
+    {
+         check = true;
+    }
+    else if((char)(lowerlater[i]) == 64) 
+    {
+        check = true;
+    }
+    else if((char)(lowerlater[i]) == 46)
+    {
+         check = true;
+    }
+    else
+    {
+        check = false;
+        temp += email[i];
+        break;
+    }
+}
+Console.WriteLine(lowerlater);
+if (check == true)
+    Console.WriteLine("Good");
+else 
+    Console.WriteLine(temp);
+
---
+++ password_generate/Program.cs
@@ -0,0 +1,46 @@
+﻿bool choose;
+Console.WriteLine("sonlar qatnashsinmi: y/n");
+char choosenumber= char.Parse(Console.ReadLine());
+
+Console.WriteLine("harflar qatnashsinmi: y/n");
+char chooseletters = char.Parse(Console.ReadLine());
+
+Console.WriteLine("boshqa simvollar qatnashsinmi: y/n");
+char chooseOtherSimvols = char.Parse(Console.ReadLine());
+
+Console.WriteLine("password uzunligi: ");
+int lengthPassword = int.Parse(Console.ReadLine());
+
+Random random = new Random();
+string str = "";
+if (choosenumber == 'y')
+{
+    choose = true;
+}
+else if (choosenumber == 'n')
+{
+    choose = false;
+}
+else
+    Console.WriteLine("invalid choose");
+ 
+for (int i  = 0; i < lengthPassword; i++)
+{
+    if (choosenumber == 'y')
+    {
+        str += (random.Next(1, 10).ToString());
+    }
+
+    if (chooseletters == 'y')
+    {
+        str += (char)(random.Next(65, 91));
+    }
+
+    if (lengthPassword == 'y')
+    {
+        str += (char)(random.Next(33, 47));
+    }
+   
+    
+}
+Console.WriteLine(str.Count());
