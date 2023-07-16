974463dee36e8ed822b375e075e58586266dba90
Asadbek Rashidov <rashidovasadbek264@gmail.com>
2023-07-16 15:37:18 +0500

    N3HT

---
+++ Essay checker/Program.cs
@@ -0,0 +1,76 @@
+﻿using System.Runtime.ConstrainedExecution;
+
+int ball = 100;
+string text = "Lorem ipsum dolor sit amet consectetur adipisicing elit.quaerat est quas commodi quibusdam labore, nihil doloribus quam temporibus inventore optio expedita consectetur voluptatem QUIDEM nulla soluta earum.Numquam rem alias minima culpa iste distinctio.Eum similique est consequuntur minus, odio nisi recusandae iure asperiores facere, reiciendis voluptate maiores!Repellat, dolorum!";
+
+if (text.Length < 500)
+{
+    ball -= 5;
+}
+
+var wordscount = 0;
+var words = text.Split(' ');
+for  (int i = 0; i < words.Length; i++)
+{
+    for (int j = 1; j < words[i].Length ; j++)
+    {
+        bool check = words[i].Contains(words[j], StringComparison.OrdinalIgnoreCase);
+        if (check)
+            wordscount++;
+    }
+    if (words.Length / 5 < wordscount)
+    {
+        //Console.WriteLine(words[i]);
+        ball -= 5;
+        break;
+    }
+ 
+}
+
+var wordsA = text.Split('.','!');
+for (int i = 0; i < wordsA.Length - 1; i++)
+{
+    bool checkA = Char.IsLower(wordsA[i].ToString(),0);
+    if (checkA)
+    {
+        //Console.WriteLine(wordsA[i][0]);
+        ball -= 5;
+    }
+}
+
+var wordsB = text.Split(' ');
+bool checkB = false;
+for (int i = 0;i < wordsB.Length; i++)
+{
+    for(int j = 0; j < wordsB[i].Length -1; j++)
+    {
+        //Console.Write(wordsB[i][j]);
+        checkB = Char.IsUpper(wordsB[i],1);   
+    }
+    if (checkB)
+    {
+        ball -= 10;
+    }
+}
+
+var wordsC = text.Split('.');
+var countC = 1;
+for(int i = 0; i < wordsC.Length; i++)
+{
+    for(var j = 0;j < wordsC[i].Length; j++)
+    {
+        if (wordsC[i][j] == ' ')
+        {
+            countC++;
+        }
+    } 
+    if(countC  > 20)
+    {
+       /* Console.WriteLine(countC);
+        Console.WriteLine(wordsC[i]);*/
+        ball -= 20;  
+    }
+    countC = 0;
+}
+
+Console.WriteLine(ball);
---
+++ Username generate/Program.cs
@@ -0,0 +1,30 @@
+﻿Console.Write("ism sharifingizni kiriting:");
+var name = Console.ReadLine();
+Random random = new Random();
+
+var strgenerate = "";
+var strsimvols = new string[] {"@","#","$","-","_"};
+
+int index = random.Next(strsimvols.Length - 1);
+strgenerate+=strsimvols[index];
+
+var usernamebaza = new string[]
+{ "Alisher-Kasmanaft","G'ayrat-bob","QodirAli123","Mamarayimlolikk","Nasiba426","Natasha990","Odil-uchar","Jumagul-maysa","Toxir-tank","Ali325"}; 
+
+for  (int i = 0; i < usernamebaza.Length; i++)
+{
+    if(name != usernamebaza[i])
+    {
+        for (int j = 0;j < 3; j++)
+        {
+           strgenerate += random.Next(1,9).ToString();
+        }
+        break;
+    }
+    else
+    {
+        Console.WriteLine(usernamebaza[i] + strsimvols[index]);break;
+    }
+}
+
+ Console.WriteLine(name+strgenerate);
