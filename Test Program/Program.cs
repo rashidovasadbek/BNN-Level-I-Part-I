b6683859d1d6c068f2f0f1387034270f23f7dff5
Asadbek Rashidov <rashidovasadbek264@gmail.com>
2023-07-16 18:20:18 +0500

    N4HT

---
+++ Test Program/Program.cs
@@ -0,0 +1,62 @@
+﻿int ball = 0;
+var test = new string[] {
+    "1.Quyish qaysi tarafdan chiqadi ?",
+    "2.Oy qanaqa shakilda ?",
+    "3.Odamning nechta qo'li bo'ladi ?",
+    "4.Tovuq birinchi paydo bo'lganmi yoki tuxum ?",
+    "5.Osmon qanaqa rangda ?"
+};
+var correctAnswer = new string[] { "g'arb", "yumaloq", "ikkita", "tovuq","ko'k" };
+var wrongAnswer = new string[] { "sharq", "tortburchak", "uchta", "tuxum","qizil"};
+var saveAnswer = new string[5];
+var savewrongAnswer = new string[5];
+var savewronganswerOftest = new string[5];
+
+
+for(int i = 0; i < test.Length; i++)
+{
+    Console.WriteLine("Question:");
+    Console.WriteLine(test[i]);
+    Console.WriteLine("A)"+correctAnswer[i]);
+    Console.WriteLine("B)"+wrongAnswer[i]+"\n");
+    Console.Write("User:");
+    var choose = Console.ReadLine();
+    Console.Write("\n");
+    
+    switch (choose)
+    {
+        case "A": saveAnswer[i] = correctAnswer[i]; break;
+        case "B": saveAnswer[i] = wrongAnswer[i]; break;
+        default:
+            Console.WriteLine("Invalid choose");break;
+    
+    }
+}
+
+for (int i = 0; i < saveAnswer.Length; i++)
+{
+    if (saveAnswer[i].Equals(correctAnswer[i]))
+    {
+        ball += 2;
+    }
+    else if (saveAnswer[i].Equals(wrongAnswer[i]))
+    {
+        savewrongAnswer[i] = saveAnswer[i];
+        savewronganswerOftest[i] = test[i];
+    }
+}
+Console.WriteLine("ball: " + ball);
+Console.WriteLine("Invalid Answers:\n");
+
+for(int i = 0;i < savewronganswerOftest.Length; i++)
+{
+    if (savewronganswerOftest[i] != null)
+    {
+        Console.Write("Question:");
+        Console.WriteLine(savewronganswerOftest[i]);
+        Console.Write("Answer:");
+        Console.WriteLine(savewrongAnswer[i]);
+    }
+   
+    
+}
---
+++ calculyator/Program.cs
@@ -0,0 +1,22 @@
+﻿while (true)
+{
+    Console.WriteLine("Enter a number:");
+    var num1 = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("Enter a command:");
+    var click = Char.Parse(Console.ReadLine());
+    Console.WriteLine("Enter a number:");
+    var num2 = Convert.ToInt32(Console.ReadLine());
+    
+    switch (click) 
+    {
+        case '*':Console.WriteLine("Result:"+(num1 * num2)); break;
+        case '/': Console.WriteLine("Result:" + (num1 / num2)); break;
+        case '-': Console.WriteLine("Result:" + (num1 - num2)); break;
+        case '+': Console.WriteLine("Result:" + (num1 + num2)); break;
+        case 'e':Console.WriteLine("Thank you for using calculator"); return;
+        default: Console.WriteLine("invalid choose:");break;
+    }
+    
+
+}
+
