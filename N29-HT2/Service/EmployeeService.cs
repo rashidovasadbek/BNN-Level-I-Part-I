24576c8f8d9aad06f029ecfce983eb6ec0f35a5b
Asadbek Rashidov <rashidovasadbek264@gmail.com>
2023-08-26 15:18:18 +0500

    N29_HT2

---
+++ N29-HT2/Service/EmployeeService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Mail;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace N29_HT2.Service
+{
+    public class EmployeeService
+    {
+        public SmtpClient smtpClientInstance { get; set;}
+        public Task<bool> HireAsync( string firstname,string lastname,string emailAddress)
+        {
+            return Task.FromResult(true);
+        }
+    }
+}
