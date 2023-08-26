eb40f1393d67c0a6cc90d24622709961e3e9c0db
Asadbek Rashidov <rashidovasadbek264@gmail.com>
2023-08-26 15:17:22 +0500

    N32_T1

---
+++ takrorlash_oop/Model/ShortenedLink.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace takrorlash_oop.Model
+{
+    public class ShortenedLink
+    {
+        public int Id {  get; set; }    
+        public string OriginalUrl { get; set; }
+        public string ShortenedUrl { get; set; }
+
+        public ShortenedLink(int id, string orginalUrl, string shortenedUrl)
+        {
+            Id = id;
+            OriginalUrl = orginalUrl;
+            ShortenedUrl = shortenedUrl;
+        }
+
+        public override bool Equals(object? obj)
+        {
+            if (obj is ShortenedLink url)
+                return this.GetHashCode() == url.GetHashCode();
+            return false;
+        }
+
+        public override int GetHashCode()
+        {
+            int hash = 17;
+            hash = hash * 23 + Id.GetHashCode();
+            hash = hash * 23 + OriginalUrl.GetHashCode();
+            hash = hash * 23 + ShortenedUrl.GetHashCode();
+            return hash;    
+        }
+
+        public static bool operator == (ShortenedLink url1, ShortenedLink url2)
+        {
+            return url1.Equals(url2);
+        }
+
+        public static bool operator !=(ShortenedLink url1, ShortenedLink url2)
+        {
+            return !url1.Equals(url2);
+        }
+
+    }
+}
---
+++ takrorlash_oop/Program.cs
@@ -0,0 +1,9 @@
+﻿using takrorlash_oop.Model;
+
+ShortenedLink shortenedLinkA = new ShortenedLink(1,"url","url");
+ShortenedLink shortenedLinkB = new ShortenedLink(1,"url","url");
+if (shortenedLinkA == shortenedLinkB)
+    Console.WriteLine("Equls");
+else 
+    Console.WriteLine("not Equls");
+
