b647de789c0f81f83a4d2193edc52a62624c4bda
Asadbek Rashidov <rashidovasadbek264@gmail.com>
2023-08-26 15:17:55 +0500

    N29_HT1

--- N29-H1/Program.cs
+++ N29-H1/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+var username = new List<string> 
+{ 
+    "Boxodir007",
+    "Alish41",
+    "Ali_okeng",
+    "Tohir algo",
+    "Shomurod",
+    "Kamol_kru",
+    "Malika_03"
+};
+//userlarga file larni ochish
+var createFileTasks = username.Select(username => Task.Run(() =>
+{
+    var fileStream = File.Create($"{username.ToLower()}.docx");
+    Console.WriteLine($"{username} ga file yaratildi");
+    return fileStream;
+}));
+var userFiles = (await Task.WhenAll(createFileTasks)).ToList();
